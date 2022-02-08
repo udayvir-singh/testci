e0c7e4c6322b343476716e3c6580ab8e62e94a4a
Udayvir Singh <97400310+udayvir-singh@users.noreply.github.com>
2022-02-08 16:07:40 +0000

    [auto]: build deps

--- lua/tangerine/utils/init.lua
+++
@@ -1,5 +0,0 @@
-local modules = {p = "tangerine.utils.path", fs = "tangerine.utils.fs", df = "tangerine.utils.diff", env = "tangerine.utils.env", log = "tangerine.utils.logger", err = "tangerine.utils.error"}
-local function _1_(_241, _242)
-  return require(modules[_242])
-end
-return setmetatable({}, {__index = _1_})
